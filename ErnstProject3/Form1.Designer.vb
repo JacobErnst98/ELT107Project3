6287f280324575446b9c68cf2028e8f98f9a6a70
JacobErnst98 <jacobaernst@gmail.com>
2016-10-22 00:32:26 -0400

    Final Project

---
+++ ErnstProject3/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.groupBox3 = New System.Windows.Forms.GroupBox()
+        Me.radioButton1 = New System.Windows.Forms.RadioButton()
+        Me.radioButton2 = New System.Windows.Forms.RadioButton()
+        Me.radioButton3 = New System.Windows.Forms.RadioButton()
+        Me.groupBox2 = New System.Windows.Forms.GroupBox()
+        Me.radioButton4 = New System.Windows.Forms.RadioButton()
+        Me.radioButton5 = New System.Windows.Forms.RadioButton()
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radioButton6 = New System.Windows.Forms.RadioButton()
+        Me.radioButton7 = New System.Windows.Forms.RadioButton()
+        Me.radioButton8 = New System.Windows.Forms.RadioButton()
+        Me.textBox3 = New System.Windows.Forms.TextBox()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.groupBox3.SuspendLayout()
+        Me.groupBox2.SuspendLayout()
+        Me.groupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'button3
+        '
+        Me.button3.Location = New System.Drawing.Point(261, 245)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(75, 23)
+        Me.button3.TabIndex = 24
+        Me.button3.Text = "Exit"
+        Me.button3.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(139, 245)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(75, 23)
+        Me.button2.TabIndex = 23
+        Me.button2.Text = "Calculate"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(23, 245)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(75, 23)
+        Me.button1.TabIndex = 22
+        Me.button1.Text = "Reset"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'groupBox3
+        '
+        Me.groupBox3.Controls.Add(Me.radioButton1)
+        Me.groupBox3.Controls.Add(Me.radioButton2)
+        Me.groupBox3.Controls.Add(Me.radioButton3)
+        Me.groupBox3.Location = New System.Drawing.Point(16, 50)
+        Me.groupBox3.Name = "groupBox3"
+        Me.groupBox3.Size = New System.Drawing.Size(107, 92)
+        Me.groupBox3.TabIndex = 20
+        Me.groupBox3.TabStop = False
+        Me.groupBox3.Text = "Ohms Units"
+        '
+        'radioButton1
+        '
+        Me.radioButton1.AutoSize = True
+        Me.radioButton1.Location = New System.Drawing.Point(9, 69)
+        Me.radioButton1.Name = "radioButton1"
+        Me.radioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.radioButton1.TabIndex = 10
+        Me.radioButton1.TabStop = True
+        Me.radioButton1.Text = "MOhms"
+        Me.radioButton1.UseVisualStyleBackColor = True
+        '
+        'radioButton2
+        '
+        Me.radioButton2.AutoSize = True
+        Me.radioButton2.Location = New System.Drawing.Point(7, 46)
+        Me.radioButton2.Name = "radioButton2"
+        Me.radioButton2.Size = New System.Drawing.Size(58, 17)
+        Me.radioButton2.TabIndex = 9
+        Me.radioButton2.TabStop = True
+        Me.radioButton2.Text = "kOhms"
+        Me.radioButton2.UseVisualStyleBackColor = True
+        '
+        'radioButton3
+        '
+        Me.radioButton3.AutoSize = True
+        Me.radioButton3.Checked = True
+        Me.radioButton3.Location = New System.Drawing.Point(7, 19)
+        Me.radioButton3.Name = "radioButton3"
+        Me.radioButton3.Size = New System.Drawing.Size(52, 17)
+        Me.radioButton3.TabIndex = 8
+        Me.radioButton3.TabStop = True
+        Me.radioButton3.Text = "Ohms"
+        Me.radioButton3.UseVisualStyleBackColor = True
+        '
+        'groupBox2
+        '
+        Me.groupBox2.Controls.Add(Me.radioButton4)
+        Me.groupBox2.Controls.Add(Me.radioButton5)
+        Me.groupBox2.Location = New System.Drawing.Point(136, 62)
+        Me.groupBox2.Name = "groupBox2"
+        Me.groupBox2.Size = New System.Drawing.Size(103, 63)
+        Me.groupBox2.TabIndex = 21
+        Me.groupBox2.TabStop = False
+        Me.groupBox2.Text = "Amp Units"
+        '
+        'radioButton4
+        '
+        Me.radioButton4.AutoSize = True
+        Me.radioButton4.Location = New System.Drawing.Point(12, 40)
+        Me.radioButton4.Name = "radioButton4"
+        Me.radioButton4.Size = New System.Drawing.Size(51, 17)
+        Me.radioButton4.TabIndex = 7
+        Me.radioButton4.TabStop = True
+        Me.radioButton4.Text = "Amps"
+        Me.radioButton4.UseVisualStyleBackColor = True
+        '
+        'radioButton5
+        '
+        Me.radioButton5.AutoSize = True
+        Me.radioButton5.Checked = True
+        Me.radioButton5.Location = New System.Drawing.Point(12, 19)
+        Me.radioButton5.Name = "radioButton5"
+        Me.radioButton5.Size = New System.Drawing.Size(40, 17)
+        Me.radioButton5.TabIndex = 6
+        Me.radioButton5.TabStop = True
+        Me.radioButton5.Text = "mA"
+        Me.radioButton5.UseVisualStyleBackColor = True
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.radioButton6)
+        Me.groupBox1.Controls.Add(Me.radioButton7)
+        Me.groupBox1.Controls.Add(Me.radioButton8)
+        Me.groupBox1.Location = New System.Drawing.Point(254, 50)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(107, 103)
+        Me.groupBox1.TabIndex = 19
+        Me.groupBox1.TabStop = False
+        Me.groupBox1.Text = "What would you like to calculate?"
+        '
+        'radioButton6
+        '
+        Me.radioButton6.AutoSize = True
+        Me.radioButton6.Location = New System.Drawing.Point(7, 75)
+        Me.radioButton6.Name = "radioButton6"
+        Me.radioButton6.Size = New System.Drawing.Size(71, 17)
+        Me.radioButton6.TabIndex = 10
+        Me.radioButton6.TabStop = True
+        Me.radioButton6.Text = "Restance"
+        Me.radioButton6.UseVisualStyleBackColor = True
+        '
+        'radioButton7
+        '
+        Me.radioButton7.AutoSize = True
+        Me.radioButton7.Location = New System.Drawing.Point(7, 52)
+        Me.radioButton7.Name = "radioButton7"
+        Me.radioButton7.Size = New System.Drawing.Size(51, 17)
+        Me.radioButton7.TabIndex = 9
+        Me.radioButton7.TabStop = True
+        Me.radioButton7.Text = "Amps"
+        Me.radioButton7.UseVisualStyleBackColor = True
+        '
+        'radioButton8
+        '
+        Me.radioButton8.AutoSize = True
+        Me.radioButton8.Checked = True
+        Me.radioButton8.Location = New System.Drawing.Point(7, 29)
+        Me.radioButton8.Name = "radioButton8"
+        Me.radioButton8.Size = New System.Drawing.Size(48, 17)
+        Me.radioButton8.TabIndex = 8
+        Me.radioButton8.TabStop = True
+        Me.radioButton8.Text = "Volts"
+        Me.radioButton8.UseVisualStyleBackColor = True
+        '
+        'textBox3
+        '
+        Me.textBox3.Location = New System.Drawing.Point(261, 12)
+        Me.textBox3.Name = "textBox3"
+        Me.textBox3.Size = New System.Drawing.Size(100, 20)
+        Me.textBox3.TabIndex = 18
+        '
+        'textBox2
+        '
+        Me.textBox2.Location = New System.Drawing.Point(139, 12)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(100, 20)
+        Me.textBox2.TabIndex = 17
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(23, 12)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(100, 20)
+        Me.textBox1.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 25
+        Me.Label1.Text = "Restance"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(145, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "Corrent in amps"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(293, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 27
+        Me.Label3.Text = "Voltage"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 287)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.groupBox3)
+        Me.Controls.Add(Me.groupBox2)
+        Me.Controls.Add(Me.groupBox1)
+        Me.Controls.Add(Me.textBox3)
+        Me.Controls.Add(Me.textBox2)
+        Me.Controls.Add(Me.textBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.groupBox3.ResumeLayout(False)
+        Me.groupBox3.PerformLayout()
+        Me.groupBox2.ResumeLayout(False)
+        Me.groupBox2.PerformLayout()
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents button3 As Button
+    Private WithEvents button2 As Button
+    Private WithEvents button1 As Button
+    Private WithEvents groupBox3 As GroupBox
+    Private WithEvents radioButton1 As RadioButton
+    Private WithEvents radioButton2 As RadioButton
+    Private WithEvents radioButton3 As RadioButton
+    Private WithEvents groupBox2 As GroupBox
+    Private WithEvents radioButton4 As RadioButton
+    Private WithEvents radioButton5 As RadioButton
+    Private WithEvents groupBox1 As GroupBox
+    Private WithEvents radioButton6 As RadioButton
+    Private WithEvents radioButton7 As RadioButton
+    Private WithEvents radioButton8 As RadioButton
+    Private WithEvents textBox3 As TextBox
+    Private WithEvents textBox2 As TextBox
+    Private WithEvents textBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ ErnstProject3/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Dim V, I, R As Decimal
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles button3.Click
+        Close()
+    End Sub
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        radioButton3.Checked = True
+        radioButton5.Checked = True
+        radioButton8.Checked = True
+        textBox1.Text = ""
+        textBox2.Text = ""
+        textBox3.Text = ""
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        If radioButton8.Checked = True Then
+            If radioButton3.Checked = True Then
+                R = Val(textBox1.Text)
+            ElseIf radioButton2.Checked = True Then
+                R = Val(textBox1.Text) * 1000
+            ElseIf radioButton1.Checked = True Then
+                R = Val(textBox1.Text) * 1000000000
+            End If
+            If radioButton5.Checked = True Then
+                I = Val(textBox2.Text) * 0.001
+            ElseIf radioButton4.Checked = True Then
+                I = Val(textBox2.Text)
+            End If
+            If R <= 0 Or I <= 0 Then
+                MsgBox("Please enter only numric values larger than 0")
+                Exit Sub
+            End If
+            If IsNumeric(textBox1.Text) And IsNumeric(textBox2.Text) Then
+                If R > 0 And I > 0 Then
+                    V = I * R
+                    textBox3.Text = V
+                End If
+            End If
+        End If
+        If radioButton7.Checked = True Then
+            V = Val(textBox3.Text)
+            If radioButton3.Checked = True Then
+                R = Val(textBox1.Text)
+            ElseIf radioButton2.Checked = True Then
+                R = Val(textBox1.Text) * 1000
+            ElseIf radioButton1.Checked = True Then
+                R = Val(textBox1.Text) * 1000000000
+            End If
+            If R <= 0 Or V <= 0 Then
+                MsgBox("Please enter only numric values larger than 0")
+                Exit Sub
+            End If
+            If IsNumeric(textBox1.Text) And IsNumeric(textBox3.Text) Then
+                If R > 0 And V > 0 Then
+                    I = V / R
+                    If radioButton5.Checked = True Then
+                        I = I * 1000
+                    End If
+                    textBox2.Text = I
+                End If
+            End If
+        End If
+        If radioButton6.Checked = True Then
+            If radioButton5.Checked = True Then
+                I = Val(textBox2.Text) * 0.001
+            ElseIf radioButton4.Checked = True Then
+                I = Val(textBox2.Text)
+            End If
+            V = Val(textBox3.Text)
+            If V <= 0 Or I <= 0 Then
+                MsgBox("Please enter only numric values larger than 0" & V & I)
+                Exit Sub
+            End If
+            If V > 0 And I > 0 Then
+                If IsNumeric(textBox2.Text) And IsNumeric(textBox3.Text) Then
+                    R = V / I
+                    If radioButton2.Checked = True Then
+                        R = R * 0.001
+                    ElseIf radioButton1.Checked = True Then
+                        R = R * 0.000001
+                    End If
+                    textBox1.Text = R
+                End If
+            End If
+        End If
+    End Sub
+End Class
